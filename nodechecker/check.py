05671b0798e75bac4bee3aac32e7f49559722865
Joakim Nyman <joakim.nyman@vidhance.com>
2022-01-03 19:16:27 +0100

    Fixed so that the timeout value is actually working.

--- nodechecker/check.py
+++ nodechecker/check.py
@@ -55,21 +55,19 @@ def check_nodes(config):
 
 def check_websocket_nodes(config):
   async def test_connection():
-    async with websockets.connect('wss://' + node[1], timeout=30) as websocket:
-        await websocket.send("")
+    await asyncio.wait_for(websockets.connect('wss://' + node[1]), timeout=3)
   for node in config.items('WebsocketNodes'):
     # RPC node sometimes fails to open a websocket connection with log message:
     # "Unable to build WebSocket connection WS Error <Capacity>: Unable to add another connection to the event loop"
     # Assuming this is normal for a websocket service, retry a few times before reporting.
-    max_tries = 10
+    max_tries = 5
     for current_try in range(max_tries):
       try:
         asyncio.get_event_loop().run_until_complete(test_connection())
         print("SUCCESS connecting to endpoint {} ({})".format(node[0], node[1]), flush=True)
         break
-      except Exception as e:
+      except Exception:
         print("FAILED connecting to endpoint {} ({})".format(node[0], node[1]), flush=True)
-        print(e, flush=True)
         if current_try == max_tries-1:
           report_incident(node, config)
         else:
